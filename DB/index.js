eb5ef122e8e92e3f9c353cf52a4a517dca433f00
Evgvakh <evgvakh@gmail.com>
2023-04-12 20:56:26 +0200

    dsa

--- DB/index.js
+++ DB/index.js
@@ -1,16 +1,16 @@
-// export const dbParams = {
-//   host: "sql.freedb.tech",
-//   user: "freedb_EvgVakh",
-//   database: "freedb_gemgarden",
-//   password: "k8NUQ2PQA@mS$BP",
-// };
-
 export const dbParams = {
-  host: "localhost",
-  user: "root",
-  database: "gems",
-  password: "",
+  host: "sql.freedb.tech",
+  user: "freedb_EvgVakh",
+  database: "freedb_gemgarden",
+  password: "k8NUQ2PQA@mS$BP",
 };
+
+// export const dbParams = {
+//   host: "localhost",
+//   user: "root",
+//   database: "gems",
+//   password: "",
+// };
 
 
 
